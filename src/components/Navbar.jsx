433e3a1cd00248581340ee99257f3ff8875a4229
Toto <toto@Totos-MacBook-Pro.local>
2024-11-18 12:25:01 +0700

    NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import logo from '../assets/react.svg'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <img src={logo} alt="" width="50px" />
             <ul>
-                <Link to="/"><li>Home</li></Link>
-                <Link to="/product"><li>Product</li></Link>
-                <Link to="/about"><li>About</li></Link>
-                <Link to="/contact"><li>Contact</li></Link>
+                <NavLink to="/"><li>Home</li></NavLink>
+                <NavLink to="/product"><li>Product</li></NavLink>
+                <NavLink to="/about"><li>About</li></NavLink>
+                <NavLink to="/contact"><li>Contact</li></NavLink>
             </ul>
             <button>Get Started</button>
         </div>
